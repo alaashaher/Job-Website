3c8da38a159da384f582fa7f79444d8e03ccc7f7
alaashaher <alaashaher12@gmail.com>
2019-02-13 22:30:25 +0200

    mobile menu

--- Jop-Project/script/index.js
+++ Jop-Project/script/index.js
@@ -1,1 +1,9 @@
-console.log('worked well');
+//to open nav bar in mobile
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+//to close nav bar in mobile
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+console.log('worked');
--- dist/js/index.js
+++ dist/js/index.js
@@ -1,2 +1,2 @@
-console.log("worked well");
+function openNav(){document.getElementById("mySidenav").style.width="250px"}function closeNav(){document.getElementById("mySidenav").style.width="0"}console.log("worked");
 //# sourceMappingURL=index.js.map
